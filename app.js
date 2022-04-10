0e331803c025e2a53b38172958f688082fdc749c
Nikodem Dabski <nikodemdabski@gmail.com>
2022-04-10 19:52:08 +0200

    Test for manual approver

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Going through the CI/CD tutorial'));
+app.get('/', (req, res) => res.send('Hello World! Going through the CI/CD tutorial - manual approve from CodePipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
